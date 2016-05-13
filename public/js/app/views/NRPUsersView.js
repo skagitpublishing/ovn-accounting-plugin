f61d75b97682caf9e11ecc3d94b7ffaa72ec8263
christroutner <chris.troutner@gmail.com>
2016-05-13 09:48:12 -0700

    Adding new user creation form to NRP User View

--- public/js/app/views/NRPUsersView.js
+++ public/js/app/views/NRPUsersView.js
@@ -120,6 +120,7 @@ define([
         global.csrfToken = csrfToken;
         
         //Show the new user form
+        $('#csrfTokenInputForm').show();
         $('#newUserForm').show();
         
         debugger;
