c4332ea6dbcccdc73bc3fd3d3be1c2611f7ee9fa
Chris Troutner <chris.troutner@gmail.com>
2016-10-14 07:38:04 -0700

    Adding path for /logwork

---
+++ public/js/serversettings.js
@@ -0,0 +1,34 @@
+/*
+ * This file is the central location for setting the server info. It is set up to work with both vanilla JS files as well as AMD/Require.js enabled JS files.
+ */ 
+
+function getServerData() {
+  
+  //These settings are for the ConnextCMS Demo site. Change them to reflect your own server.
+  var serverData = {
+   
+    //Basic server IP and port for KeystoneJS/ConnextCMS
+    serverIp: '107.170.255.157',
+    serverPort: '80', //Not Used
+    
+    //Mail settings
+    mailPort: '8888',
+    adminEmail: 'chris.troutner@gmail.com',
+    debugEmail: 'chris.troutner@gmail.com',
+    additionalEmails: '',
+    
+    //Section ID used to implement the Private Pages feature.
+    privatePagesSection: ""
+  }
+
+  return serverData; 
+
+};
+
+
+
+//This little bit of code handles AMD enabled JS files that expect a define() function.
+if ( typeof(define) === "function" && define.amd ) {
+  define([], getServerData );  
+}
+
