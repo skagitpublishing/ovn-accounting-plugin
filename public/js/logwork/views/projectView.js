6819fb727f5606d6e2a75ec99f995a615ad20f7b
Chris Troutner <chris.troutner@gmail.com>
2016-10-16 01:18:18 -0700

    creating project view - pie chart

--- public/js/logwork/views/projectView.js
+++ public/js/logwork/views/projectView.js
@@ -97,31 +97,29 @@ define([
       
       var ctx = this.$el.find('#pieChart');
       var myChart = new Chart(ctx, {
-          type: 'bar',
+          type: 'pie',
           data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }]
+            labels: [
+                "Red",
+                "Blue",
+                "Yellow"
+            ],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
           },
+          /*
           options: {
               scales: {
                   yAxes: [{
@@ -131,6 +129,7 @@ define([
                   }]
               }
           }
+          */
       });
     }
     
