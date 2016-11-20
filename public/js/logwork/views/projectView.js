d1d999d3ff15e42c38077922f490690eee7a08fb
Chris Troutner <chris.troutner@gmail.com>
2016-11-20 13:49:20 -0800

    Adding horizontal bar chart to project view

--- public/js/logwork/views/projectView.js
+++ public/js/logwork/views/projectView.js
@@ -242,6 +242,45 @@ define([
                 }]
           },
       });
+      
+      
+      //http://www.chartjs.org/docs/#bar-chart
+      var ctx3 = this.$el.find('#testChart');
+      
+      //Remove any previously created chart.
+      if(this.testChart != undefined)
+        this.testChart.destroy();
+      
+      this.testChart = new Chart(ctx3, {
+          type: 'horizontalBar',
+          data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                }
+            ]
+          },
+      });
+      
     },
     
     //This function expects an array of LogWork model GUIDs.
