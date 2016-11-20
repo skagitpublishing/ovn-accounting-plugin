47dedb784bea5618d07999cf704e599d5157d504
Chris Troutner <chris.troutner@gmail.com>
2016-11-20 14:29:22 -0800

    Adding horizontal bar chart to project view

--- public/js/logwork/views/projectView.js
+++ public/js/logwork/views/projectView.js
@@ -296,6 +296,7 @@ define([
             scales: {
               xAxes: [{
                 display: true,
+                stacked: true,
                 ticks: {
                   beginAtZero: true
                 }
