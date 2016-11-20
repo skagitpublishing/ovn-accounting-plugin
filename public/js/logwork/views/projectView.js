a04c7c3bfec7954b3c8198c852ebd2ede5d72361
Chris Troutner <chris.troutner@gmail.com>
2016-11-20 14:43:12 -0800

    Adding horizontal bar chart to project view

--- public/js/logwork/views/projectView.js
+++ public/js/logwork/views/projectView.js
@@ -334,15 +334,15 @@ define([
             labels: ["2014", "2013", "2012", "2011"],
 
             datasets: [{
-                data: [727, 589, 537, 543, 574],
+                data: [727, 589, 537, 543],
                 backgroundColor: "rgba(63,103,126,1)",
                 hoverBackgroundColor: "rgba(50,90,100,1)"
             },{
-                data: [238, 553, 746, 884, 903],
+                data: [238, 553, 746, 884],
                 backgroundColor: "rgba(163,103,126,1)",
                 hoverBackgroundColor: "rgba(140,85,100,1)"
             },{
-                data: [1238, 553, 746, 884, 903],
+                data: [1238, 553, 746, 884],
                 backgroundColor: "rgba(63,203,226,1)",
                 hoverBackgroundColor: "rgba(46,185,235,1)"
             }]
