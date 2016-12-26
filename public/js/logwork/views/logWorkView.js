e7adc96fb738a78c980b27162de4a0b2797220fe
Chris Troutner <chris.troutner@gmail.com>
2016-12-26 08:37:38 -0800

    Adding last 5 work entries at bottom of logWorkView.js

--- public/js/logwork/views/logWorkView.js
+++ public/js/logwork/views/logWorkView.js
@@ -485,6 +485,23 @@ define([
       }
       
       debugger;
+      
+      // temporary array holds objects with position and sort-value
+      var mapped = startTimeArray.map(function(el, i) {
+        return { index: i, value: el };
+      })
+      
+      // sorting the mapped array containing the reduced values
+      mapped.sort(function(a, b) {
+        return +(a.value > b.value) || +(a.value === b.value) - 1;
+      });
+      
+      // container for the resulting order
+      var result = mapped.map(function(el){
+        return startTimeArray[el.index];
+      });
+      
+      debuggger;
     }
     
 	});
