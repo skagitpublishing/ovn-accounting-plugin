3fd2d3c55c879b09ca0bf3a823503528f5a26685
Chris Troutner <chris.troutner@gmail.com>
2016-12-27 12:23:21 -0800

    Updating User API and converting create user account page to Keystone View.

--- plugin/rpiovn-plugin/keystone/routes/views/createaccount.js
+++ plugin/rpiovn-plugin/keystone/routes/views/createaccount.js
@@ -6,7 +6,8 @@ exports = module.exports = function(req, res) {
         var locals = res.locals;
 
         locals.user = req.user;
-        locals.user.password = ""; //Blank out password for security purposes.
+        if(locals.user != undefined)
+          locals.user.password = ""; //Blank out password for security purposes.
 
         // Set locals
         locals.section = 'createaccount';
